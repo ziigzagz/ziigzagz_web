7ac103cacb774133454bd83720006955c8ab965c
ziigzagz <iffan.h@ideabranch.co.th>
2023-10-29 22:10:38 +0700

    add ga

--- js.php
+++ js.php
@@ -12,11 +12,11 @@
  <!-- Template Main JS File -->
  <script src="assets/js/main.js"></script>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XK5KPLPNX5"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1XW6580P7R"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-XK5KPLPNX5');
+  gtag('config', 'G-1XW6580P7R');
 </script>
