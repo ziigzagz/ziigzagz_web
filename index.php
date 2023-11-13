bc723354ecadaa143a990bdb1e91973f1fb3c7f6
ziigzagz <iffan.h@ideabranch.co.th>
2023-11-13 12:33:15 +0700

    remove chat support

--- index.php
+++ index.php
@@ -22,22 +22,6 @@
     gtag('config', 'G-1XW6580P7R');
   </script>
 
-  
-  <!--Start of Tawk.to Script-->
-  <script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-      Tawk_LoadStart = new Date();
-    (function() {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = 'https://embed.tawk.to/6551a672cec6a912820f30a3/1hf3f4cmd';
-      s1.charset = 'UTF-8';
-      s1.setAttribute('crossorigin', '*');
-      s0.parentNode.insertBefore(s1, s0);
-    })();
-  </script>
-  <!--End of Tawk.to Script-->
 </head>
 
 <body>
